ed5ad8eadec2818db5bce74496c05977d3644999
Vu Thanh Bay <vuthanhbayit@gmail.com>
2021-11-26 13:56:37 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ x }}
+    {{ y }}
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import { useMouse  } from '@vueuse/core'
+
+export default defineComponent({
+  setup() {
+    return {
+      ...useMouse()
+    }
+  }
+})
+</script>
